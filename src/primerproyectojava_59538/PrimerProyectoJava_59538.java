885fa89ee13d6e4e5db9d3c409fa0e5df910fcd0
Personal <Personal@Familia>
2021-03-01 17:11:58 -0600

    NumLetras59538

---
+++ src/primerproyectojava_59538/PrimerProyectoJava_59538.java
@@ -0,0 +1,25 @@
+/*
+ * Lenguajes de programación I
+ * Facultad de Ingeniería
+ * Universidad Autónoma de Campeche
+ */
+package primerproyectojava_59538;
+
+/**
+ *
+ * @author JossmarOswaldoKuCupul
+ */
+public class PrimerProyectoJava_59538 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Declarar tres variables
+        int entero = 18;
+        char letra = 'j';
+        boolean aprobar = true;
+    }
+    
+}
